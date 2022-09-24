f64791b3ad5e18e68c1655b739002543270a9c47
Eren Yeger <nurske@gmail.com>
2022-09-25 04:23:14 +0600

    added new overlay

--- starter/src/app.js
+++ starter/src/app.js
@@ -23,12 +23,105 @@ async function initMap() {
 
     let personName = "dev7";
 
-    await initWebGLOverlayView1(map, people[personName]);
-
+    await initWebGLOverlayView(map, people[personName]);
 })();
 
 
 let set = new Set();
+let setPath = new Set();
+
+async function initWebGLOverlayView(map, person) {
+    let startTime = new Date();
+    let tmpTime = startTime.getTime() + 1e9;
+    for (let location of person) {
+        startTime.setTime(Math.min(startTime.getTime(), tmpTime + location.timestamp ));
+    }
+
+    person.sort((a, b) => {
+       return a.timestamp - b.timestamp;
+    });
+
+    console.log(person);
+
+    let scene, renderer, camera, loader;
+
+    // WebGLOverlayView code goes here
+    const webGLOverlayView = new google.maps.WebGLOverlayView();
+
+    webGLOverlayView.onAdd = () => {
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera();
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.75); // soft white light
+        scene.add(ambientLight);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
+        directionalLight.position.set(0.5, -1, 0.5);
+        scene.add(directionalLight);
+
+        loader = new GLTFLoader();
+        const source = 'pin.gltf';
+
+        loader.load(source, gltf => {
+            gltf.scene.scale.set(5, 5, 5);
+            gltf.scene.rotation.x = 180 * Math.PI / 180;
+            scene.add(gltf.scene);
+        });
+    }
+
+    webGLOverlayView.onContextRestored = ({gl}) => {
+        renderer = new THREE.WebGLRenderer({
+            canvas: gl.canvas, context: gl, ...gl.getContextAttributes(),
+        });
+
+        renderer.autoClear = false;
+    }
+
+    webGLOverlayView.onDraw = ({gl, transformer}) => {
+        const curTime = new Date().getTime() - startTime.getTime();
+        for (let i = 0; i + 1 < person.length; i++) {
+            if (person[i].timestamp < curTime && curTime < person[i + 1].timestamp) {
+                const dx = person[i + 1].longitude - person[i].longitude;
+                const dy = person[i + 1].latitude - person[i].latitude;
+                const dz = person[i + 1].altitude - person[i].altitude;
+                const dt = person[i + 1].timestamp - person[i].timestamp;
+                const xSpeed = dx / dt;
+                const ySpeed = dy / dt;
+                const zSpeed = dz / dt;
+
+                const dtCur = curTime - person[i].timestamp;
+
+                const obj = {   lat: person[i].latitude + ySpeed * dtCur,
+                                lng: person[i].longitude + xSpeed * dtCur,
+                                altitude: person[i].altitude + zSpeed * dtCur}
+
+                const matrix = transformer.fromLatLngAltitude(obj);
+                camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
+                webGLOverlayView.requestRedraw();
+                renderer.render(scene, camera);
+            }
+        }
+
+        person.forEach(loc => {
+            const obj = {lat: loc.latitude, lng: loc.longitude, altitude: loc.altitude}
+
+
+            const res = (new Date().getTime() - startTime.getTime()) >= loc.timestamp;
+
+            if (res) {
+                const matrix = transformer.fromLatLngAltitude(obj);
+                camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
+                webGLOverlayView.requestRedraw();
+                renderer.render(scene, camera);
+                if (!set.has(loc)) {
+                    map.panTo(obj);
+                    set.add(loc);
+                }
+            }
+        });
+
+        renderer.resetState();
+    }
+    webGLOverlayView.setMap(map);
+}
 
 async function initWebGLOverlayView1(map, person) {
     let startTime = new Date();
